5f1e14491f3b7e660cd21ea5be56c225d4cf62d4
SkreimOF <2001main2001@gmail.com>
2022-04-06 01:52:19 +0200

    Do While до 3

--- mod4.1/Program.cs
+++ mod4.1/Program.cs
@@ -22,38 +22,30 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        for (int i = 1; i < 5; i++)
+        Console.WriteLine("Цикл while");
+        int k = 0;
+        do
         {
-            Console.WriteLine("Iteration {0}", i);
+            Console.WriteLine(k);
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
             switch (Console.ReadLine())
             {
-                case "red":
+                case "Red":
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
+                    Console.WriteLine("Your color on the skreen!");
                     break;
                 default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is yellow!");
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Your color on the skreen!");
                     break;
             }
+
+    k++;
+
         }
+            while (k < 3) ;
     }
 }
